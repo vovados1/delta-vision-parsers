d2c5a6789b39f3598ba267223652501786a167fb
vovados1 <doskovol1@gmail.com>
2026-01-02 01:24:50 -0600

    Add Kraken parser

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { BinanceParser } from "./parsers/impl/BinanceParser"
 import { ByBitParser } from "./parsers/impl/ByBitParser"
 import { OkxParser } from "./parsers/impl/OkxParser"
 import { CoinbaseParser } from "./parsers/impl/CoinbaseParser"
+import { KrakenParser } from "./parsers/impl/KrakenParser"
 
 const binanceParser = new BinanceParser({
   pair: "btcusdt",
@@ -37,4 +38,13 @@ const coinbaseParser = new CoinbaseParser({
   },
 })
 
-coinbaseParser.connect()
+// coinbaseParser.connect()
+
+const krakenParser = new KrakenParser({
+  pair: "BTC/USD",
+  onDatapoint: (datapoint) => {
+    console.log("Kraken", datapoint)
+  },
+})
+
+krakenParser.connect()
---
+++ src/parsers/impl/KrakenParser.ts
@@ -0,0 +1,43 @@
+import { BaseParser } from "../BaseParser"
+import { ParserConfig } from "../types"
+
+type KrakenDatapoint = [number, string[], string, string]
+
+export class KrakenParser extends BaseParser<KrakenDatapoint> {
+  public static readonly EXCHANGE_NAME = "Kraken"
+
+  protected readonly WS_URL = `wss://ws.kraken.com`
+
+  constructor(config: ParserConfig) {
+    const onOpen = () => {
+      if (!this.ws) return
+      this.ws.send(
+        JSON.stringify({
+          event: "subscribe",
+          pair: [this.config.pair],
+          subscription: {
+            name: "spread",
+          },
+        })
+      )
+      config.onOpen?.()
+    }
+
+    super(
+      { ...config, onOpen },
+      (data) => {
+        return {
+          bid: Number(data[1][0]),
+          ask: Number(data[1][1]),
+          bidQty: Number(data[1][3]),
+          askQty: Number(data[1][4]),
+          timestamp: Number(data[1][2]),
+        }
+      },
+      (msg) => {
+        const obj = msg as object | Array<KrakenDatapoint>
+        return Array.isArray(obj)
+      }
+    )
+  }
+}
